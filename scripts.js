698b1e29258386ac5520c81f7322e3b5a6810373
trungnguyen <trungn21@student.ubc.ca>
2024-11-17 01:23:52 -0800

    fix mobile

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ fetch('data.json')
             // Create experience card
             const experienceCard = document.createElement('div');
             experienceCard.classList.add('experience');
+            experienceCard.setAttribute('id', 'experience');
             experienceCard.innerHTML = `
                 <div class="company-logo">
                     <img src="${experience.logo}" alt="${experience.company}" class="logo">
@@ -19,20 +20,31 @@ fetch('data.json')
                             <h3>${experience.title}</h3>
                             <p>${experience.company}</p>                    
                         </div>
-                        <div style="text-align:end">
+                        <div class="right" style="text-align:end">
                             <p style="color: #aaa">${experience.start} - ${experience.end}, ${experience.year}</p>
                             <p style="color: #aaa">${experience.location}</p>
                         </div>
                     </div>
                     <ul class="experience-description"></ul>
+                    <div class="experience-tags"></div>
                 </div>
             `;
+
             experience.description.forEach(desc => {
                 const descEl = document.createElement('li');
                 descEl.textContent = desc;
                 experienceCard.querySelector('.experience-description').appendChild(descEl);
             });
+
+            experience.tags.forEach(tag => {
+                const tagEl = document.createElement('span');
+                tagEl.classList.add('tag');
+                tagEl.textContent = tag;
+                experienceCard.querySelector('.experience-tags').appendChild(tagEl);
+            });
+
             experienceContainer.appendChild(experienceCard);
+            
         });
     })
 
